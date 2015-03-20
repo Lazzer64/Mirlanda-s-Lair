793d7d411aa6999788f2422dd20c3cf2a64b1ef6
Alex <ajpele@gmail.com>
2015-03-20 14:38:55 -0700

    Added missing files

---
+++ src/Splash.java
@@ -0,0 +1,33 @@
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+
+public class Splash extends GamePanel {
+
+	int currx = 0;
+	
+	public void paint(Graphics g){
+//		g.drawImage(Images.dungeon_splash, currx, 0, null);
+//		try {
+//			Thread.sleep(300);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			currx++;
+//			e.printStackTrace();
+//		}
+////		repaint();
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
